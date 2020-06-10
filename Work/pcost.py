4039b1310a3a8fa840f035c891bd9d92cbdfebc9
singkia <singkia@github.com>
2020-06-11 02:00:10 +0800

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,39 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+total_cost = 0.0
+
+with open('C:/Users/LO/Documents/GitHub/practical-python/Work/Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost += nshares * price
+
+    print('Total cost', total_cost)
+
+
+
+
+
+'''
+import os
+
+total_cost = 0
+s_cost = 0
+
+with open('C:/Users/LO/Documents/GitHub/practical-python/Work/Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        s_cost = float(row[1]) * float(row[2])
+        total_cost += s_cost
+        s_cost = 0
+
+    print('Total cost', total_cost)
+
+    
+'''
